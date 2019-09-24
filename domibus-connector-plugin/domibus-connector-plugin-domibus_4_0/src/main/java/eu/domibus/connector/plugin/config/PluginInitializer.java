ea363a3963f719e1bc78859d26fd75ad65ab0067
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2019-09-24 17:15:33 +0200

    initial work on better seperation between domibus and domibusConnectorPlugin

--- domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/main/java/eu/domibus/connector/plugin/config/PluginInitializer.java
+++ domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/main/java/eu/domibus/connector/plugin/config/PluginInitializer.java
@@ -1,4 +1,23 @@
 package eu.domibus.connector.plugin.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.annotation.PostConstruct;
+
 public class PluginInitializer {
+
+    @Autowired
+    public ApplicationContext context;
+
+    @PostConstruct
+    public void init() {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]
+                {"classpath:eu/domibus/connector/plugin/domibus-connector-plugin-child-context.xml"},
+                context);
+        Object domibusConnectorWebservice = classPathXmlApplicationContext.getBean("domibusConnectorWebservice");
+
+    }
+
 }
---
+++ domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/test/java/eu/domibus/connector/plugin/config/PluginInitializerTest.java
@@ -0,0 +1,7 @@
+package eu.domibus.connector.plugin.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PluginInitializerTest {
+
+}
