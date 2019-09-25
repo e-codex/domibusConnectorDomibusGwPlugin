af5567b0274440426efdb70ea494ab4a5083896d
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2019-09-25 13:10:54 +0200

    work on child context for gw plugin

--- domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/main/java/eu/domibus/connector/plugin/config/PluginInitializer.java
+++ domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/main/java/eu/domibus/connector/plugin/config/PluginInitializer.java
@@ -1,5 +1,6 @@
 package eu.domibus.connector.plugin.config;
 
+import eu.domibus.connector.plugin.ws.DomibusConnectorWebservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,13 +12,29 @@ public class PluginInitializer {
     @Autowired
     public ApplicationContext context;
 
+    private DomibusConnectorWebservice domibusConnectorWebservice;
+
     @PostConstruct
     public void init() {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]
                 {"classpath:eu/domibus/connector/plugin/domibus-connector-plugin-child-context.xml"},
                 context);
-        Object domibusConnectorWebservice = classPathXmlApplicationContext.getBean("domibusConnectorWebservice");
-
+        this.domibusConnectorWebservice = classPathXmlApplicationContext.getBean("domibusConnectorWebservice", DomibusConnectorWebservice.class);
     }
 
+    public ApplicationContext getContext() {
+        return context;
+    }
+
+    public void setContext(ApplicationContext context) {
+        this.context = context;
+    }
+
+    public DomibusConnectorWebservice getDomibusConnectorWebservice() {
+        return domibusConnectorWebservice;
+    }
+
+    public void setDomibusConnectorWebservice(DomibusConnectorWebservice domibusConnectorWebservice) {
+        this.domibusConnectorWebservice = domibusConnectorWebservice;
+    }
 }
--- domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/test/java/eu/domibus/connector/plugin/config/PluginInitializerTest.java
+++ domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/test/java/eu/domibus/connector/plugin/config/PluginInitializerTest.java
@@ -1,7 +1,20 @@
 package eu.domibus.connector.plugin.config;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PluginInitializerTest {
+public class PluginInitializerTest {
+
+
+    @Test
+    public void testPluginInit() {
+        PluginInitializer pluginInitializer = new PluginInitializer();
+        pluginInitializer.init();
+
+        assertThat(pluginInitializer.getDomibusConnectorWebservice()).isNotNull();
+
+    }
 
 }
