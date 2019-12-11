3c82e1bfe14f7fe09a191e82a8b8dfca4dc90150
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2019-12-11 15:12:44 +0100

    revert to 4.1.0-RELASE - last revision is corrupted

--- domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/main/java/eu/domibus/connector/plugin/config/PluginInitializer.java
+++ domibus-connector-plugin/domibus-connector-plugin-domibus_4_0/src/main/java/eu/domibus/connector/plugin/config/PluginInitializer.java
@@ -1,45 +1,4 @@
 package eu.domibus.connector.plugin.config;
 
-import eu.domibus.connector.plugin.ws.DomibusConnectorWebservice;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import javax.annotation.PostConstruct;
-
 public class PluginInitializer {
-
-    @Autowired
-    public ApplicationContext context;
-
-    private DomibusConnectorWebservice domibusConnectorWebservice;
-//    private Object connectorDeliveryWebserviceNotificationListenerService;
-
-    @PostConstruct
-    public void init() {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]
-                {"classpath:eu/domibus/connector/plugin/domibus-connector-plugin-child-context.xml"},
-                context);
-        this.domibusConnectorWebservice = classPathXmlApplicationContext.getBean("domibusConnectorWebservice", DomibusConnectorWebservice.class);
-//        this.connectorDeliveryWebserviceNotificationListenerService = classPathXmlApplicationContext.getBean("connectorDeliveryWebserviceNotificationListenerService");
-
-    }
-
-
-    public ApplicationContext getContext() {
-        return context;
-    }
-
-    public void setContext(ApplicationContext context) {
-        this.context = context;
-    }
-
-    public DomibusConnectorWebservice getDomibusConnectorWebservice() {
-        return domibusConnectorWebservice;
-    }
-
-    public void setDomibusConnectorWebservice(DomibusConnectorWebservice domibusConnectorWebservice) {
-        this.domibusConnectorWebservice = domibusConnectorWebservice;
-    }
 }
