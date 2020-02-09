9da7f1e146dd3620ebb5ac81ddfb83b4d477d4f5
Stephan Sp <spind42@gmail.com>
2020-02-09 21:30:47 +0100

    itplugin

---
+++ domibus-connector-plugin/domibus-gw-connector-plugin-ittest/src/main/java/eu/domibus/connector/plugin/DomibusGwPluginITCase.java
@@ -0,0 +1,14 @@
+package eu.domibus.connector.plugin;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class DomibusGwPluginITCase {
+
+
+    @BeforeAll
+    public static void beforeAll() {
+
+    }
+
+
+}
